ec1326c80a1fd4b11c5c660f2b12f9a592c8126b
Shubham Kunal <shubhamkunal19@gmail.com>
2023-01-05 03:57:27 +0530

    Update users.js

--- server/models/users.js
+++ server/models/users.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false);
-mongoose.connect('mongodb://127.0.0.1:27017/todoList');
+mongoose.connect(mongodb+srv://shubhamkunal:zac@cluster0.7t8gyby.mongodb.net/todoList');
 
 const UserSchema = new mongoose.Schema({
     username: {type:String,unique:true, required:true,maxLength:12},
