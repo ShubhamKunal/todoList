cd502b8a2f08850d7a6538e3b3c3d8c062cf9918
Shubham Kunal <shubhamkunal19@gmail.com>
2023-01-07 23:35:20 +0530

    Create Loading.js

---
+++ frontend/src/MyComponents/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Loading = () => {
+  return (
+    <div>
+      <div>Loading...</div>
+      <div>
+        <img src="/loading.gif" alt="Loading animation" />
+      </div>
+    </div>
+  )
+}
